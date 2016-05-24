036d0da39697902ecea7a4d491524cc81d4dbb30
Ariel Bl <arielb3330@gmail.com>
2016-05-25 01:00:25 +0300

    groups list

---
+++ app/src/main/java/com/example/zakiva/outalk/GroupListAdapter.java
@@ -0,0 +1,81 @@
+package com.example.zakiva.outalk;
+
+/**
+ * Created by zakiva on 5/12/16.
+ */
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.firebase.client.Query;
+
+public class GroupListAdapter extends FirebaseListAdapter<GroupInfo> {
+
+    private String mUsername;
+    private Context context;
+
+    // The mUsername for this client. We use this to indicate which messages originated from this user
+
+    public GroupListAdapter(Query ref, Activity activity, int layout, String mUsername) {
+        super(ref, GroupInfo.class, layout, activity);
+        this.mUsername = mUsername;
+        this.context = activity;
+    }
+
+    /**
+     * Bind an instance of the <code>Post</code> class to our view. This method is called by <code>FirebaseListAdapter</code>
+     * when there is a data change, and we are given an instance of a View that corresponds to the layout that we passed
+     * to the constructor, as well as a single <code>Post</code> instance that represents the current data to bind.
+     *
+     * @param view A view instance corresponding to the layout we passed to the constructor.
+     * @param groupInfo An instance representing the current state of a chat message
+     */
+    @Override
+    protected void populateView(View view, GroupInfo groupInfo) {
+        final String groupName = groupInfo.getName();
+        Button groupNameButton = (Button) view.findViewById(R.id.groupNameList);
+        groupNameButton.setText(groupName);
+        groupNameButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent chat = new Intent(context, Chat.class);
+                chat.putExtra("username", mUsername);
+                chat.putExtra("name", groupName);
+                context.startActivity(chat);
+            }
+        });
+
+        RelativeLayout relativeLayout = (RelativeLayout) view.findViewById(R.id.groupElement);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        /*
+        // If the message was sent by this user, color it differently
+        if (author != null && author.equals(mUsername)) {
+            authorText.setVisibility(View.GONE);
+            relativeLayout.setBackgroundColor(Color.parseColor("#ffffff"));
+            params.setMargins(40, 3, 0, 3);
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            relativeLayout.setLayoutParams(params);
+            //  authorText.setTextColor(Color.RED);
+            //   linearLayout.setGravity(Gravity.RIGHT); // no
+        } else {
+            authorText.setVisibility(View.VISIBLE);
+            relativeLayout.setBackgroundColor(Color.parseColor("#9a48f84b"));
+            params.setMargins(0, 3, 40, 3);
+            params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            relativeLayout.setLayoutParams(params);
+            // authorText.setTextColor(Color.BLUE);
+        }
+        ((TextView) view.findViewById(R.id.message)).setText(" " + post.getMessage() + "   ");
+        */
+    }
+}
--- app/src/main/java/com/example/zakiva/outalk/Groups.java
+++ app/src/main/java/com/example/zakiva/outalk/Groups.java
@@ -1,29 +1,55 @@
 package com.example.zakiva.outalk;
 
+import android.app.ListActivity;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.firebase.client.Firebase;
 
-public class Groups extends AppCompatActivity {
+public class Groups extends ListActivity {
+
     private Firebase firebase;
+    String username;
+    String groupName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
         firebase = new Firebase("https://vivid-inferno-9487.firebaseio.com/");
+        final Bundle extras = getIntent().getExtras();
+        username = extras.getString("username");
+        setListeners();
+
+    }
+
+    void setListeners() {
+
+        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
+        final ListView listView = getListView();
+        // Tell our list adapter that we only want 50 messages at a time
+        final GroupListAdapter mGroupListAdapter = new GroupListAdapter(firebase.child("groups").child("list of groups"), this, R.layout.group_element, username);
+        listView.setAdapter(mGroupListAdapter);
+        mGroupListAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(mGroupListAdapter.getCount() - 1);
+            }
+        });
     }
 
     public void buttonCreateGroupClicked(View view) {
         // Pass arguments to chat
-        final Bundle extras = getIntent().getExtras();
-        String username = extras.getString("username");
+
         EditText inputText = (EditText) findViewById(R.id.groupName);
-        String groupName = inputText.getText().toString();
+        groupName = inputText.getText().toString();
         Intent chat = new Intent(Groups.this, Chat.class);
         chat.putExtra("username", username);
         chat.putExtra("name", groupName);
@@ -31,8 +57,10 @@ public class Groups extends AppCompatActivity {
         GroupInfo groupInfo = new GroupInfo(groupName, "1");
         Post post = new Post("Welcome to the group!", "Groupy");
         firebase.child("groups").child(groupName).child("info").push().setValue(groupInfo);
+        firebase.child("groups").child("list of groups").push().setValue(groupInfo);
         firebase.child("groups").child(groupName).child("posts").push().setValue(post);
 
         startActivity(chat);
     }
+
 }
